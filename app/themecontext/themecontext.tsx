f41fefdeb3d140e8489880f9fde57875e8e950f8
Demar English <demarenglish@Mac.attlocal.net>
2026-08-15 11:20:32 -0400

    Updates.

--- app/themecontext/themecontext.tsx
+++ app/themecontext/themecontext.tsx
@@ -1,7 +1,18 @@
 "use client";
-import { createContext } from "react";
 
-const ThemeContext = createContext({
+import { createContext, ReactNode } from "react";
+
+type ThemeProviderType = {
+    children: ReactNode;
+    handleDarkMode: () => void;
+};
+
+type ThemeContextType = {
+    darkMode: boolean;
+    handleDarkMode: () => void;
+};
+
+const ThemeContext = createContext<ThemeContextType>({
     darkMode: true,
     handleDarkMode: () => {},
 });
