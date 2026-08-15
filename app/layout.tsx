da7edd3c829f3c4d9129df5fe908b01e13180ec4
Demar English <demarenglish@Mac.attlocal.net>
2026-08-15 11:19:55 -0400

    Update to context provider.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,38 @@
 import type { Metadata } from "next";
-import { Poppins, Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "./themecontext/themeprovider";
-
 
 const poppins = Poppins({
   variable: "--font-poppins",
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
-})
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: [
+    "100",
+    "200",
+    "300",
+    "400",
+    "500",
+    "600",
+    "700",
+    "800",
+    "900",
+  ],
 });
 
 export const metadata: Metadata = {
   title: "Billy English | Software Engineer",
-  description: "Software Engineer Porfolio",
+  description: "Software Engineer Portfolio",
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
-
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html
       lang="en"
-      className={`${poppins.variable} ${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${poppins.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <ThemeProvider>
