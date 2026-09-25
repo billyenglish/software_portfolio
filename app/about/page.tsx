d61e3695e0365ca45fffeb7c847eaa099edb8c23
Demar English <demarenglish@Demars-MacBook-Pro.local>
2026-09-25 11:14:52 -0400

    Create about section.

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Nav from "../components/navigation";
+import Footer from "../components/footer";
+
+export default function About() {
+    return (
+        <>
+            <Nav />
+            <section>
+                <h2>About</h2>
+            </section>
+            <Footer />
+        </>
+    );
+};
+
