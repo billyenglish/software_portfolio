5b7168a2516273178a5016d2823d2c146c5f4832
Demar English <demarenglish@Demars-MacBook-Pro.local>
2026-09-25 11:15:38 -0400

    Reworked the project section.

---
+++ app/projects/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import Nav from "../components/navigation";
+import Footer from "../components/footer";
+
+export default function Projects() {
+
+    return (
+        <>
+            <Nav />
+            <section
+                className="
+                    border
+                    h-screen
+                    w-screen
+                "
+            >
+                <h2>Project</h2>
+            </section>
+            <Footer />
+        </>
+    );
+};
